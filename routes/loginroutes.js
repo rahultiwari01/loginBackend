c0c6e65a36d298b0debd01c38da72ce972ddc5c3
Rahul Tiwari <rahulti011@gmail.com>
2020-05-25 18:03:07 +0530

    user registration api done

---
+++ routes/loginroutes.js
@@ -0,0 +1,81 @@
+
+
+var express    = require("express");
+ var mysql      = require('mysql');
+ var connection = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'rahul',
+   password : 'admin',
+   database : 'users'
+ });
+ var app = express();
+ 
+ connection.connect(function(err){
+ if(!err) {
+     console.log("Database is connected ... \n\n");  
+ } else {
+     console.log("Error connecting database ... \n\n");  
+ }
+ });
+
+ exports.register = async function(req,res){
+    const password = req.body.password;
+    const bcrypt = require('bcrypt');
+    const saltRounds = 10;
+    const encryptedPassword = await bcrypt.hash(password, saltRounds)
+    var users={
+       "email":req.body.email,
+       "password":encryptedPassword
+     }
+    
+    connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+      if (error) {
+        res.send({
+          "code":400,
+          "failed":"error ocurred"
+        })
+      } else {
+        res.send({
+          "code":200,
+          "success":"user registered sucessfully"
+            });
+        }
+    });
+  };
+
+  exports.login = async function(req,res){
+    var email= req.body.email;
+    var password = req.body.password;
+    connection.query('SELECT * FROM users WHERE email = ?',[email], async function (error, results, fields) {
+      if (error) {
+        res.send({
+          "code":400,
+          "failed":"error ocurred"
+        })
+      }else{
+        if(results.length >0){
+          const comparision = await bcrypt.compare(password, results[0].password)
+          if(comparision){
+              res.send({
+                "code":200,
+                "success":"login sucessfull"
+              })
+          }
+          else{
+            res.send({
+                 "code":204,
+                 "success":"Email and password does not match"
+            })
+          }
+        }
+        else{
+          res.send({
+            "code":206,
+            "success":"Email does not exits"
+              });
+        }
+      }
+      });
+  };
+
+app.listen(3000);
